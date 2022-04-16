13d77643ee375ca64781e9fd672ef9f2b33c0e9b
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-04-16 16:07:32 +0200

    More sophisticated program
    Taken from go playground

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-    fmt.Println("hello containers")
+	for i := 0; i < 10; i++ {
+		dur := time.Duration(rand.Intn(1000)) * time.Millisecond
+		fmt.Printf("Sleeping for %v\n", dur)
+		// Sleep for a random duration between 0-1000ms
+		time.Sleep(dur)
+	}
+	fmt.Println("Done!")
 }
